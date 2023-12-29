5598e0f4fc17f6fdfdbd85bfaea62f11fcd4ec99
G.S.Sanjay <110849848+sanjay021104gs@users.noreply.github.com>
2023-12-29 20:12:45 +0530

    Update Number_game.java

--- Number_game.java
+++ Number_game.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class Number_game{
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
